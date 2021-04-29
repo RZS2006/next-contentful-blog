bd3ad8a9474ba29ba985da51e91e0b0e7b99219a
Raiyan Shamim <razash139@gmail.com>
2021-04-29 18:29:02 +0300

    Style About page

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Navbar = () => {
 	return (
 		<nav className="bg-gray-100 w-full">
-			<div className="wrapper py-4 flex flex-col md:flex-row gap-4">
+			<div className="wrapper py-4 flex flex-col sm:flex-row gap-4">
 				<span className="font-semibold text-green-800">Mönkijä Blogit</span>
 				<div className="flex flex-wrap gap-4">
 					<Link href={'/posts'}>
--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,28 @@
+import Head from 'next/head';
+
 const AboutPage = () => {
 	return (
-		<div>
-			<h1 className="text-2xl font-semibold mb-6">About Mönkijä Blogit</h1>
-			<p className="w-1/2">
-				This blog was made as a tribute to the tragic events at Hirvensalo
-				on the 27th of March, 2021. The Polaris Ranger traktorimönkijä had a
-				short but eventful life. Rest in peace. The recycled metal will be
-				put into good use.
-			</p>
-		</div>
+		<>
+			<Head>
+				<title>About | Mönkijä Blogit</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+			</Head>
+			<div>
+				<div className="wrapper py-6">
+					<h1 className="">About Mönkijä Blogit</h1>
+					<p className="lg:w-1/2 leading-8">
+						This blog was made as a tribute to the tragic events at
+						Hirvensalo on the 27th of March, 2021. The Polaris Ranger{' '}
+						<em>traktorimönkijä</em> had a short but eventful life. Rest
+						in peace. The recycled metal will be put into good use. Let us
+						hope that the <em>mopoauto</em> will last longer.
+					</p>
+				</div>
+			</div>
+		</>
 	);
 };
 
