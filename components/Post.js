366ee2217a9a811665f4983478fe4fcd524570c1
Raiyan Shamim <razash139@gmail.com>
2021-04-30 18:24:26 +0300

    Style Posts page

--- components/Post.js
+++ components/Post.js
@@ -6,29 +6,37 @@ const Post = ({ post }) => {
 	const { createdAt } = post.sys;
 
 	return (
-		<div className="my-6 flex w-full sm:hover:shadow-md sm:hover:bg-gray-50 sm:transition sm:duration-200 sm:ease-in-out">
-			{featuredImage && (
-				<Image
-					src={`https:${featuredImage.fields.file.url}`}
-					alt={featuredImage.fields.title}
-					height={150}
-					width={220}
-					layout="intrinsic"
-					className="object-cover m-0 p-0"
-				/>
-			)}
-			<div className="py-2 px-5 sm:py-4 flex flex-col sm:max-w-sm">
-				<h2 className="text-xl font-semibold mb-1">{title}</h2>
-				<p className="hidden sm:block text-gray-600">{snippet}</p>
-				<div className="flex items-end h-full">
-					<div className="text-xs text-gray-600">
-						<span>By {author}</span>
-						{' ∙ '}
-						<span>{new Date(createdAt).toDateString()}</span>
+		<Link href={`/posts/${slug}`}>
+			<a>
+				<div className="my-6 flex w-full sm:border sm:border-transparent sm:hover:border-gray-100 sm:hover:bg-gray-50 sm:transition sm:duration-200 sm:ease-in-out">
+					<Image
+						src={
+							featuredImage
+								? `https:${featuredImage.fields.file.url}`
+								: '/default-featured-image.svg'
+						}
+						alt={featuredImage ? featuredImage.fields.title : title}
+						height={150}
+						width={220}
+						layout="intrinsic"
+						className="object-cover"
+					/>
+					<div className="py-2 px-5 sm:py-4 flex flex-col w-full sm:max-w-md">
+						<h2 className="text-lg sm:text-xl font-semibold mb-1">
+							{title}
+						</h2>
+						<p className="hidden sm:block text-gray-600">{snippet}</p>
+						<div className="flex items-end h-full">
+							<div className="text-xs text-gray-600">
+								<span>By {author}</span>
+								{' ∙ '}
+								<span>{new Date(createdAt).toDateString()}</span>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			</a>
+		</Link>
 	);
 };
 
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,5 @@
 import { createClient } from 'contentful';
+import Head from 'next/head';
 import Post from '../../components/Post';
 
 export const getStaticProps = async () => {
@@ -19,12 +20,25 @@ export const getStaticProps = async () => {
 
 const PostsPage = ({ posts }) => {
 	return (
-		<div>
-			<h1 className="text-2xl font-semibold mb-6">Posts Page</h1>
-			{posts.map(post => (
-				<Post key={post.sys.id} post={post} />
-			))}
-		</div>
+		<>
+			<Head>
+				<title>Posts | Mönkijä Blogit</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+			</Head>
+			<div>
+				<div className="wrapper py-6">
+					<h1>Recent Posts</h1>
+					<div>
+						{posts.map(post => (
+							<Post key={post.sys.id} post={post} />
+						))}
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
